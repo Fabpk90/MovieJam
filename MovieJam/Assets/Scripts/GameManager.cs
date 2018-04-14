c7b0b82b6d8ada4dd5470a98989a7896623b3857
Unknown <pifi77@hotmail.fr>
2018-04-14 04:46:40 +0200

    My part (gameManager mostly)

---
+++ MovieJam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance = null;
+
+    [SerializeField]
+    private GameObject playerGO;
+
+    public Player[] players = new Player[4];
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && players[0] == null)
+        {
+            players[0] = Instantiate(playerGO, this.transform.position, this.transform.rotation).GetComponent<Player>();
+        }
+    }
+}
